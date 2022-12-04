fix edge sharpness calculation

edges_sharpness.py
@@ -5,40 +5,58 @@ from unique_vertices import unique_edges as calc_unique_edges
 from utils import recursive_list, list_contains
 
 
-def calc_edge_sharpness(faces, normals):
+def calc_edge_sharpness(faces):
     """
     :param faces: list of faces, each of which is a list containing all its vertices
     :param normals: list of normals for each face
     :returns: dictionary with sharpness value from 0 to 1 for each unique edge given in faces
     """
     faces = recursive_list(faces)
-    normals = recursive_list(normals)
     unique_edges = calc_unique_edges(faces)
     sharpness_values = {
         tuple(map(lambda vertex: tuple(vertex), edge)):
-            calc_single_edge_sharpness(edge, faces, normals)
+            calc_single_edge_sharpness(edge, faces)
         for edge in unique_edges
     }
     return sharpness_values
 
 
-def calc_single_edge_sharpness(edge, faces, normals):
-    normals_of_faces_with_edge = []
-    for face, normal in zip(faces, normals):
-        matching_vertices = list(filter(lambda vertex: list_contains(edge, vertex), face))
-        if len(matching_vertices) == 2:
-            normals_of_faces_with_edge.append(normal)
+def calc_single_edge_sharpness(edge, faces):
+    projected_vectors_of_faces_with_edge = []
+    a_x, a_y, a_z = edge[0]
+    b_x, b_y, b_z = edge[1]
+    for face in faces:
+        vertices_match = list(map(lambda vertex: list_contains(edge, vertex), face))
+        matching_vertices = list(filter(lambda vertex: vertex, vertices_match))
+        non_matching_vertex = face[vertices_match.index(False)]
+        c_x, c_y, c_z = non_matching_vertex
+        if len(matching_vertices) != 2:
+            continue
 
-    if len(normals_of_faces_with_edge) > 2:
+        t = ((c_x - a_x) * (b_x - a_x) + (c_y - a_y) * (b_y - a_y) + (c_z - a_z) * (b_z - a_z)) \
+            / \
+            ((b_x - a_x) ** 2 + (b_y - a_y) ** 2 + (b_z - a_z) ** 2)
+        intersection = [
+            a_x + t * (b_x - a_x),
+            a_y + t * (b_y - a_y),
+            a_z + t * (b_z - a_z),
+        ]
+        projected_vector = list(np.subtract(non_matching_vertex, intersection))
+        projected_vectors_of_faces_with_edge.append(projected_vector)
+
+    if len(projected_vectors_of_faces_with_edge) > 2:
         raise Exception('More than two faces with the same edge! What the fridge!')
     # edge only in one face -> consider edge sharp
-    elif len(normals_of_faces_with_edge) < 2:
+    elif len(projected_vectors_of_faces_with_edge) < 2:
         return 1
 
-    normal_angle = angle_between_vectors_anchor(normals_of_faces_with_edge[0], normals_of_faces_with_edge[1])
+    normal_angle = angle_between_vectors_anchor(
+        projected_vectors_of_faces_with_edge[0],
+        projected_vectors_of_faces_with_edge[1]
+    )
 
     # in case of single flipped angle
     while normal_angle >= 180:
         normal_angle -= 180
 
-    return np.interp(normal_angle, (0, 180), (0, 1))
+    return np.interp(normal_angle, (0, 180), (1, 0))