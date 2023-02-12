fix another bug with triangle area calculation

surface_sampling/points_on_surface_utils.py
@@ -17,22 +17,31 @@ def is_not_near_points(point, points, distance):
 
 
 def excluded_area_on_face(face, points, distance):  # Estimates area (likely overestimates)
+    area = 0
     overlapped_area = 0
     circle_area = math.pi * distance ** 2
     face = recursive_list(face)
     edges = calc_unique_edges([face], True)
-    points_on_edges = [
-        point
-        for points_on_edge in [filter(lambda point: point_is_on_line_segment(point, edge), points) for edge in edges]
-        for point in points_on_edge
-    ]
+    points_on_edges = list(filter(lambda point: any([point_is_on_line_segment(point, edge) for edge in edges]), points))
     # remove duplicate points (points that were found for two or more sides)
     points_on_edges = list(set(recursive_tuple(points_on_edges)))
-    for point in points_on_edges:
+    for index, point in enumerate(points_on_edges):
         for vertex in face:
-            if np.linalg.norm(np.subtract(vertex, point)) < distance / 2:
-                overlapped_area += circle_area * .25
-            else:
-                overlapped_area += circle_area * .5
+            is_corner = np.linalg.norm(np.subtract(vertex, point)) < distance / 2
+            area += circle_area * (.25 if is_corner else .5)
+            for point_b in points_on_edges[index + 1:]:
+                overlapped_area += overlap_area(point_b, point, distance)  # shouldn't be this much, maybe divide by 2
+    return area - overlapped_area
 
-    return overlapped_area
+
+def overlap_area(p1, p2, r):
+    distance = np.linalg.norm(np.subtract(p2, p1))
+    if distance > 2 * r:
+        return 0
+    elif distance == 0:
+        return math.pi * r ** 2
+    else:
+        d = distance
+        part1 = r ** 2 * math.acos((d ** 2) / (2 * d * r))
+        part3 = 0.5 * math.sqrt((-d + 2 * r) * (d + 2 * r) * d ** 2)
+        return part1 * 2 - part3